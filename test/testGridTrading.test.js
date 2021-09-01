5baef41c7ff9168ba02e4736f56623fe14f6704c
Kalzak <kalzakdev@protonmail.com>
2021-09-01 21:18:13 +0800

    Refactored testGridTrading.test.js

--- test/testGridTrading.test.js
+++ test/testGridTrading.test.js
@@ -1,5 +1,20 @@
 const GridTrading = artifacts.require("GridTrading");
 
+/*
+ * Naming convention for token variables:
+ * a = account
+ * T = token
+ * It = Incoming trade
+ * Ot = Outgoing trade
+ *
+ * eg: a0T = all owned tokens by account 0
+ * eg: a0T0 = account 0, token 0
+ * eg: a1It3 = account 1, incoming trade 3
+ * eg: a1It = all incoming trades to the account 1
+ * eg: a2Ot0 = account 2, outgonig trade 0
+ * eg: a2Ot = all outgoing trades from the account 2
+ */
+
 contract("GridTrading", (accounts) => {
 	it("sendTradeOffer creates an entry in incomingTrades for the recipient", async() => {
 		const instance = await GridTrading.deployed();
@@ -7,27 +22,24 @@ contract("GridTrading", (accounts) => {
 		await instance.generateGrid({from: accounts[0], value: 20000000000000000});
 		await instance.generateGrid({from: accounts[1], value: 20000000000000000});
 		// Get tokenIds for grids on accounts
-		let a0tokenIds = await instance.getOwnedTokens(accounts[0]);
-		let a1tokenIds = await instance.getOwnedTokens(accounts[1]);
-		let a0token0IdBN = BigInt(a0tokenIds[0]);
-		let a1token0IdBN = BigInt(a1tokenIds[0]);
+		let a0T = await getOwnedTokens(instance, accounts[0]);
+		let a1T = await getOwnedTokens(instance, accounts[1]);
 		// Start a trade
-		await instance.sendTradeOffer(accounts[1], 0, a0token0IdBN.toString(), a1token0IdBN.toString(), {from: accounts[0]});
-		let incoming = await instance.getIncomingTrades(accounts[1]);
-		assert.equal(incoming.length, 1, "New trade did not update the incomingTrade array");
+		await instance.sendTradeOffer(accounts[1], 0, a0T[0], a1T[0], {from: accounts[0]});
+		let a1It = await getIncomingTrades(instance, accounts[1]);
+		assert.equal(a1It.length, 1, "New trade did not update the incomingTrade array");
 	});
 
 	it("sendTradeOffer creates an entry in outgoingTrades for the sender", async() => {
 		const instance = await GridTrading.deployed();
-		let outgoing = await instance.getOutgoingTrades(accounts[0]);
-		assert.equal(outgoing.length, 1, "New trade did not update the outgoingTrade array");
+		let a0Ot = await getOutgoingTrades(instance, accounts[0]);
+		assert.equal(a0Ot.length, 1, "New trade did not update the outgoingTrade array");
 	});
 
 	it("sendTradeOffer creates an entry in the trades mapping", async() => {
 		const instance = await GridTrading.deployed();
-		let tradeId = await instance.getIncomingTrades(accounts[1]);
-		let tradeIdBN = BigInt(tradeId);
-		let tradeObject = await instance.getTradeDetails(tradeIdBN.toString());
+		let a1It = await instance.getIncomingTrades(accounts[1]);
+		let tradeObject = await instance.getTradeDetails(a1It[0]);
 		// Confirm that the trade is correct by seeing if the sender is the same
 		assert.equal(tradeObject.sender, accounts[0], "Sender in trade object does not match");
 	});
@@ -35,93 +47,83 @@ contract("GridTrading", (accounts) => {
 	it("withdrawTradeOffer removes the entry in outgoingTrades and incomingTrades when there is 1 trade", async() => {
 		let instance = await GridTrading.deployed();
 		// Get the tradeId of the current trade to be removed
-		let outgoingTrades = await instance.getOutgoingTrades(accounts[0]);
-		assert.equal(outgoingTrades.length, 1, "Expected initial value of outgoingTrades is incorrect");
-		let incomingTrades = await instance.getIncomingTrades(accounts[1]);
-		assert.equal(incomingTrades.length, 1, "Expected initial value of incoming is incorrect");
-		let incomingTrade = BigInt(incomingTrades[0]);
+		let a0Ot = await getOutgoingTrades(instance, accounts[0]);
+		let a1It = await getIncomingTrades(instance, accounts[1]);
+		assert.equal(a0Ot.length, 1, "Expected initial value of account0's outgoingTrades is incorrect");
+		assert.equal(a1It.length, 1, "Expected initial value of account1's incomingTrades is incorrect");
 		// Withdraw the trade
-		await instance.withdrawTradeOffer(incomingTrade.toString(), {from: accounts[0]});
+		await instance.withdrawTradeOffer(a0Ot[0], {from: accounts[0]});
 		// Check the length of lists has changes
-		outgoingTrades = await instance.getOutgoingTrades(accounts[0]);
-		assert.equal(outgoingTrades.length, 0, "Expected value is not 0");
-		incomingTrades = await instance.getIncomingTrades(accounts[1]);
-		assert.equal(incomingTrades.length, 0, "Expected value is not 0");
+		a0Ot = await getOutgoingTrades(instance, accounts[0]);
+		a1It = await getIncomingTrades(instance, accounts[1]);
+		assert.equal(a0Ot.length, 0, "Expected value of account0's outgoingTrades is not 0");
+		assert.equal(a1It.length, 0, "Expected value of account1's incomingTrades is not 0");
+
 	});
 
 	it("withdrawTradeOffer removes the entry in outgoingTrades and incomingTrades when there is >1 trades", async() => {
 		let instance = await GridTrading.deployed();
-		// Create two trades	
+		// Generate tokens needed for trade	
 		await instance.generateGrid({from: accounts[1], value: 20000000000000000});
 		await instance.generateGrid({from: accounts[2], value: 20000000000000000});
-		let a0tokenIds = await instance.getOwnedTokens(accounts[0]);
-		let a1tokenIds = await instance.getOwnedTokens(accounts[1]);
-		let a2tokenIds = await instance.getOwnedTokens(accounts[2]);
-		let a0token0IdBN = BigInt(a0tokenIds[0]);
-		let a1token0IdBN = BigInt(a1tokenIds[0]);
-		let a1token1IdBN = BigInt(a1tokenIds[1]);
-		let a2token0IdBN = BigInt(a2tokenIds[0]);
-		await instance.sendTradeOffer(accounts[1], 0, a0token0IdBN.toString(), a1token0IdBN.toString(), {from: accounts[0]});
-		await instance.sendTradeOffer(accounts[1], 0, a2token0IdBN.toString(), a1token1IdBN.toString(), {from: accounts[2]});
-		// Get the tradeId of the current trade to be removed
-		let incomingTrades = await instance.getIncomingTrades(accounts[1]);
-		assert.equal(incomingTrades.length, 2, "Expected initial value of incoming is incorrect");
-		let incomingTrade = BigInt(incomingTrades[0]);
+		// Get tokens owned by 3 users
+		let a0T = await getOwnedTokens(instance, accounts[0]);
+		let a1T = await getOwnedTokens(instance, accounts[1]);
+		let a2T = await getOwnedTokens(instance, accounts[2]);
+		// Send trade offers
+		await instance.sendTradeOffer(accounts[1], 0, a0T[0], a1T[0], {from: accounts[0]});
+		await instance.sendTradeOffer(accounts[1], 0, a2T[0], a1T[1], {from: accounts[2]});
+		// Get the trade ID of the trade to be removed
+		let a1It = await getIncomingTrades(instance, accounts[1]);
+		assert.equal(a1It.length, 2, "Expected initial value of incoming is incorrect");
 		// Withdraw the trade
-		await instance.withdrawTradeOffer(incomingTrade.toString(), {from: accounts[0]});
-		// Check the length of lists has changes
-		incomingTrades = await instance.getIncomingTrades(accounts[1]);
-		assert.equal(incomingTrades.length, 1, "Expected value is not 0");
-		// Withdraw trade at the end
-		incomingTrade = BigInt(incomingTrades[0]);
-		await instance.withdrawTradeOffer(incomingTrade.toString(), {from: accounts[2]});
+		await instance.withdrawTradeOffer(a1It[0], {from: accounts[0]});
+		// Check if the number of incoming trades has reduced to 1
+		a1It = await getIncomingTrades(instance, accounts[1]);
+		assert.equal(a1It.length, 1, "Expected the number of incomingTrades to account1 to be 1");
+		// Clean up by withdrawing the remaining trade
+		await instance.withdrawTradeOffer(a1It[0], {from: accounts[2]});
+
 	});
 
 	it("acceptTradeOffer on a grid swaps the grids between users", async() => {
 		let instance = await GridTrading.deployed();
-	
-		let a0Ts = await getOwnedTokens(instance, accounts[0]);
-		let a1Ts = await getOwnedTokens(instance, accounts[1]);
-		let a2Ts = await getOwnedTokens(instance, accounts[2]);
-		// Get the two tokenIds that will be exchanged
-		let a0tokenIds = await instance.getOwnedTokens(accounts[0]);
-		let a1tokenIds = await instance.getOwnedTokens(accounts[1]);
-		let a0token0IdBN = BigInt(a0tokenIds[0]);
-		let a1token0IdBN = BigInt(a1tokenIds[1]);
+		// Get the tokenIds that will be exchanged	
+		let a0T = await getOwnedTokens(instance, accounts[0]);
+		let a1T = await getOwnedTokens(instance, accounts[1]);
 		// Start a trade
-		await instance.sendTradeOffer(accounts[1], 0, a0token0IdBN.toString(), a1token0IdBN.toString(), {from: accounts[0]});
-		// Get the tradeId for the trade that was just made
-		let incomingTrades = await instance.getIncomingTrades(accounts[1]);
-		let incomingTrade = BigInt(incomingTrades[0]);
-		let tradeData = await instance.getTradeDetails(incomingTrade);
-		let a0t0Owner = await instance.ownerOf(a0token0IdBN.toString());
-		let a1t0Owner = await instance.ownerOf(a1token0IdBN.toString());
+		await instance.sendTradeOffer(accounts[1], 0, a0T[0], a1T[0], {from: accounts[0]});
+		// Get the trade ID for the trade that was just sent
+		let a1It = await getIncomingTrades(instance, accounts[1]);
+		// Get owner of tokens before trade
+		let t0OwnerBefore = await instance.ownerOf(a0T[0]);
+		let t1OwnerBefore = await instance.ownerOf(a1T[0]);
 		// Accept the trade
-		await instance.acceptTradeOffer(incomingTrade.toString(), {from: accounts[1]});
-		// Get owned tokens
-		let newa0t0Owner = await instance.ownerOf(a0token0IdBN.toString());
-		let newa1t0Owner = await instance.ownerOf(a1token0IdBN.toString());
-		assert.equal(a0t0Owner, newa1t0Owner, "Grids did not transfer");
-		assert.equal(a1t0Owner, newa0t0Owner, "Grids did not transfer");
-		
+		await instance.acceptTradeOffer(a1It[0], {from: accounts[1]});
+		// Get owner of tokens after trade
+		let t0OwnerAfter = await instance.ownerOf(a0T[0]);
+		let t1OwnerAfter = await instance.ownerOf(a1T[0]);
+		// Check that the ownership has changed between tokens
+		assert.equal(t0OwnerBefore, t1OwnerAfter, "Grids did not transfer");
+		assert.equal(t0OwnerAfter, t1OwnerBefore, "Grids did not transfer");
 	});
 
 	it("acceptTradeOffer cancels other incoming and outgoing tradeoffers that depend on the grid that was moved", async() => {
 		let instance = await GridTrading.deployed();
 		// Get tokenIds ofr accounts to build trades
-		let a0Ts = await getOwnedTokens(instance, accounts[0]);
-		let a1Ts = await getOwnedTokens(instance, accounts[1]);
-		let a2Ts = await getOwnedTokens(instance, accounts[2]);
+		let a0T = await getOwnedTokens(instance, accounts[0]);
+		let a1T = await getOwnedTokens(instance, accounts[1]);
+		let a2T = await getOwnedTokens(instance, accounts[2]);
 		// Construct trades
-		await instance.sendTradeOffer(accounts[1], 0, a0Ts[0], a1Ts[0], {from: accounts[0]});
-		await instance.sendTradeOffer(accounts[1], 0, a2Ts[0], a1Ts[0], {from: accounts[2]});
+		await instance.sendTradeOffer(accounts[1], 0, a0T[0], a1T[0], {from: accounts[0]});
+		await instance.sendTradeOffer(accounts[1], 0, a2T[0], a1T[0], {from: accounts[2]});
 		// Accept one of the tradeOffers
-		let a1Its = await getIncomingTrades(instance, accounts[1]);
-		let a1It0 = await instance.getTradeDetails(a1Its[0]);
-		await instance.acceptTradeOffer(a1Its[0], {from: accounts[1]});
+		let a1It = await getIncomingTrades(instance, accounts[1]);
+		let a1It0 = await instance.getTradeDetails(a1It[0]);
+		await instance.acceptTradeOffer(a1It[0], {from: accounts[1]});
 		// Check if the other trade was cancelled due to grid tokenId dependency
-		a1Its = await getIncomingTrades(instance, accounts[1]);
-		assert.equal(a1Its.length, 0, "Existing trade that had a grid tokenID dependency was not removed");
+		a1It = await getIncomingTrades(instance, accounts[1]);
+		assert.equal(a1It.length, 0, "Existing trade that had a grid tokenID dependency was not removed");
 	});
 
 });
@@ -155,6 +157,3 @@ async function getOutgoingTrades(instance, account) {
 	}
 	return tradesFormatted;
 }
-
-
-
